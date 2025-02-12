67c8c5c1830932b382c043fc4d7bde7c14468d87
Adrián Javaloy <25713831+adrianjav@users.noreply.github.com>
2025-02-12 17:54:04 +0000

    🔧 Fix imports to pass the doctests

--- causalflows/conftest.py
+++ causalflows/conftest.py
@@ -5,12 +5,18 @@ import pytest
 import torch
 import zuko
 
+from torch.distributions import ExpTransform, Gamma
+
 
 @pytest.fixture(autouse=True, scope="module")
 def doctest_imports(doctest_namespace):
     doctest_namespace["torch"] = torch
     doctest_namespace["zuko"] = zuko
     doctest_namespace["causalflows"] = causalflows
+
+    # For the examples in the doctests
+    doctest_namespace["ExpTransform"] = ExpTransform
+    doctest_namespace["Gamma"] = Gamma
 
 
 @pytest.fixture(autouse=True)
