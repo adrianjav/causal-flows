505081e869bb3676d25791392c6e96fbe3bb53d5
lkct <liangkct@yahoo.com>
2025-03-06 13:23:51 +0100

    fix scms lambda

--- causalflows/scms.py
+++ causalflows/scms.py
@@ -1,7 +1,8 @@
 r"""Helper class to create synthetic SCMs and pre-defined instances of SCMs ready to use."""
 
+from collections.abc import Callable, Sequence
 from math import sqrt
-from typing import Union
+from typing import Union, cast
 
 import torch
 import torch.nn.functional as F
@@ -11,17 +12,32 @@ from zuko.distributions import NormalizingFlow
 
 from .distributions import CausalNormalizingFlow
 
+NTen2Ten = (  # TODO type statement
+    Callable[[Tensor], Tensor]
+    | Callable[[Tensor, Tensor], Tensor]
+    | Callable[[Tensor, Tensor, Tensor], Tensor]
+    | Callable[[Tensor, Tensor, Tensor, Tensor], Tensor]
+    | Callable[[Tensor, Tensor, Tensor, Tensor, Tensor], Tensor]
+)
+Tens2Ten = Callable[[*tuple[Tensor, ...]], Tensor]  # TODO type statement
+
 
 class CausalEquations(Transform):
     domain = constraints.unit_interval
     codomain = constraints.real
     bijective = True
 
-    def __init__(self, functions, inverses, derivatives=None):
+    def __init__(
+        self,
+        functions: Sequence[Tens2Ten | NTen2Ten],
+        inverses: Sequence[Tens2Ten | NTen2Ten],
+        derivatives: Sequence[Tens2Ten | NTen2Ten] | None = None,
+    ):
         super(CausalEquations, self).__init__(cache_size=0)
-        self.functions = functions
-        self.inverses = inverses
-        self.derivatives = derivatives
+        # Must use force cast because they are incompatible
+        self.functions: Sequence[Tens2Ten] = cast(Sequence[Tens2Ten], functions)
+        self.inverses: Sequence[Tens2Ten] = cast(Sequence[Tens2Ten], inverses)
+        self.derivatives: Sequence[Tens2Ten] | None = cast(Sequence[Tens2Ten] | None, derivatives)
 
         self._interventions = dict()
 
@@ -124,13 +140,13 @@ class SCM(CausalNormalizingFlow):
 
 class Triangle(CausalEquations):
     def __init__(self, eq_type: str):
-        if eq_type == 'linear':
-            functions = [
+        if eq_type == "linear":
+            functions: list[NTen2Ten] = [
                 lambda u1: u1 + 1.0,
                 lambda x1, u2: 10 * x1 - u2,
                 lambda x1, x2, u3: 0.5 * x2 + x1 + 1.0 * u3,
             ]
-            inverses = [
+            inverses: list[NTen2Ten] = [
                 lambda x1: x1 - 1.0,
                 lambda x1, x2: (10 * x1 - x2),
                 lambda x1, x2, x3: (x3 - 0.5 * x2 - x1) / 1.0,
@@ -176,31 +192,31 @@ class Simpson(CausalEquations):
     def __init__(self, eq_type: str):
         s = torch.nn.functional.softplus
 
-        if eq_type == 'non-linear':
-            functions = [
+        if eq_type == "non-linear":
+            functions: list[NTen2Ten] = [
                 lambda u1: u1,
                 lambda x1, u2: s(1.0 - x1) + sqrt(3 / 20.0) * u2,
                 lambda x1, x2, u3: torch.tanh(2 * x2) + 3 / 2 * x1 - 1 + torch.tanh(u3),
-                lambda x1, x2, x3, u4: (x3 - 4.0) / 5.0 + 3 + 1 / sqrt(10) * u4,
+                lambda _1, _2, x3, u4: (x3 - 4.0) / 5.0 + 3 + 1 / sqrt(10) * u4,
             ]
-            inverses = [
+            inverses: list[NTen2Ten] = [
                 lambda x1: x1,
                 lambda x1, x2: 1 / sqrt(3 / 20.0) * (x2 - s(1.0 - x1)),
                 lambda x1, x2, x3: torch.atanh(x3 - torch.tanh(2 * x2) - 3 / 2 * x1 + 1),
-                lambda _, __, x3, x4: sqrt(10) * (x4 - (x3 - 4.0) / 5.0 - 3),
+                lambda _1, _2, x3, x4: sqrt(10) * (x4 - (x3 - 4.0) / 5.0 - 3),
             ]
         elif eq_type == 'sym-prod':
             functions = [
                 lambda u1: u1,
                 lambda x1, u2: 2 * torch.tanh(2 * x1) + 1 / sqrt(10) * u2,
                 lambda x1, x2, u3: 1 / 2 * x1 * x2 + 1 / sqrt(2) * u3,
-                lambda x1, x2, x3, u4: torch.tanh(3 / 2 * x1) + sqrt(3 / 10) * u4,
+                lambda x1, _2, _3, u4: torch.tanh(3 / 2 * x1) + sqrt(3 / 10) * u4,
             ]
             inverses = [
                 lambda x1: x1,
                 lambda x1, x2: sqrt(10) * (x2 - 2 * torch.tanh(2 * x1)),
                 lambda x1, x2, x3: sqrt(2) * (x3 - 1 / 2 * x1 * x2),
-                lambda x1, x2, x3, x4: 1 / sqrt(3 / 10) * (x4 - torch.tanh(3 / 2 * x1)),
+                lambda x1, _2, _3, x4: 1 / sqrt(3 / 10) * (x4 - torch.tanh(3 / 2 * x1)),
             ]
         else:
             raise ValueError(f'Equation type {eq_type} not supported.')
@@ -246,11 +262,12 @@ class LargeBackdoor(CausalEquations):
         def cdf_laplace(loc, scale, value):
             return 0.5 - 0.5 * (value - loc).sign() * torch.expm1(-(value - loc).abs() / scale)
 
-        if eq_type == 'non-linear':
-            functions = [
-                lambda u1: F.softplus(1.8 * u1) - 1,  # x1
-                lambda x1, u2: 0.25 * u2 + layer(x1, torch.zeros_like(u2)) * 1.5,  # x2
-                lambda x1, x2, u3: layer(x1, u3),  # x3
+        if eq_type == "non-linear":
+            functions: list[Tens2Ten] = [
+                lambda *args: F.softplus(1.8 * args[-1]) - 1,  # x1
+                lambda *args: 0.25 * args[-1]
+                + layer(args[0], torch.zeros_like(args[-1])) * 1.5,  # x2
+                lambda *args: layer(args[0], args[-1]),  # x3
                 lambda *args: layer(args[1], args[-1]),  # x4
                 lambda *args: layer(args[2], args[-1]),  # x5
                 lambda *args: layer(args[3], args[-1]),  # x6
@@ -260,7 +277,7 @@ class LargeBackdoor(CausalEquations):
                     -F.softplus((args[6] * 1.3 + args[7]) / 3 + 1) + 2, 0.6, args[-1]
                 ),  # x9
             ]
-            inverses = [
+            inverses: list[Tens2Ten] = [
                 lambda *args: inv_softplus(args[-1] + 1) / 1.8,  # u1
                 lambda *args: 4
                 * (-layer(args[0], torch.zeros_like(args[-1])) * 1.5 + args[-1]),  # u2
@@ -297,31 +314,31 @@ class LargeBackdoor(CausalEquations):
 
 class Fork(CausalEquations):
     def __init__(self, eq_type: str):
-        if eq_type == 'linear':
-            functions = [
+        if eq_type == "linear":
+            functions: list[NTen2Ten] = [
                 lambda u1: u1,
-                lambda _, u2: 2.0 - u2,
+                lambda _1, u2: 2.0 - u2,
                 lambda x1, x2, u3: 0.25 * x2 - 1.5 * x1 + 0.5 * u3,
-                lambda _, __, x3, u4: 1.0 * x3 + 0.25 * u4,
+                lambda _1, _2, x3, u4: 1.0 * x3 + 0.25 * u4,
             ]
-            inverses = [
+            inverses: list[NTen2Ten] = [
                 lambda x1: x1,
-                lambda _, x2: 2.0 - x2,
+                lambda _1, x2: 2.0 - x2,
                 lambda x1, x2, x3: (x3 - 0.25 * x2 + 1.5 * x1) / 0.5,
-                lambda _, __, x3, x4: (x4 - 1.0 * x3) / 0.25,
+                lambda _1, _2, x3, x4: (x4 - 1.0 * x3) / 0.25,
             ]
         elif eq_type == 'non-linear':
             functions = [
                 lambda u1: u1,
-                lambda x1, u2: u2,
+                lambda _1, u2: u2,
                 lambda x1, x2, u3: 4.0 / (1 + torch.exp(-x1 - x2)) - x2**2 + u3 / 2.0,
-                lambda x1, x2, x3, u4: 20.0 / (1 + torch.exp(x3**2 / 2 - x3)) + u4,
+                lambda _1, _2, x3, u4: 20.0 / (1 + torch.exp(x3**2 / 2 - x3)) + u4,
             ]
             inverses = [
                 lambda x1: x1,
-                lambda x1, x2: x2,
+                lambda _1, x2: x2,
                 lambda x1, x2, x3: 2.0 * (x3 - 4.0 / (1 + torch.exp(-x1 - x2)) + x2**2),
-                lambda x1, x2, x3, x4: x4 - 20.0 / (1 + torch.exp(x3**2 / 2 - x3)),
+                lambda _1, _2, x3, x4: x4 - 20.0 / (1 + torch.exp(x3**2 / 2 - x3)),
             ]
         else:
             raise ValueError(f'Equation type {eq_type} not supported.')
@@ -341,18 +358,18 @@ class Fork(CausalEquations):
 
 class Diamond(CausalEquations):
     def __init__(self, eq_type: str):
-        if eq_type == 'non-linear':
-            functions = [
+        if eq_type == "non-linear":
+            functions: list[NTen2Ten] = [
                 lambda u1: u1,
                 lambda x1, u2: x1**2 + u2 / 2,
                 lambda x1, x2, u3: x2**2 - 2.0 / (1 + torch.exp(-x1)) + u3 / 2.0,
-                lambda x1, x2, x3, u4: x3 / ((x2 + 2.0).abs() + x3 + 0.5) + u4 / 10.0,
+                lambda _1, x2, x3, u4: x3 / ((x2 + 2.0).abs() + x3 + 0.5) + u4 / 10.0,
             ]
-            inverses = [
+            inverses: list[NTen2Ten] = [
                 lambda x1: x1,
                 lambda x1, x2: 2 * (x2 - x1**2),
                 lambda x1, x2, x3: (x3 - x2**2 + 2.0 / (1 + torch.exp(-x1))) * 2.0,
-                lambda x1, x2, x3, x4: 10 * (x4 - x3 / ((x2 + 2.0).abs() + x3 + 0.5)),
+                lambda _1, x2, x3, x4: 10 * (x4 - x3 / ((x2 + 2.0).abs() + x3 + 0.5)),
             ]
         else:
             raise ValueError(f'Equation type {eq_type} not supported.')
@@ -372,15 +389,15 @@ class Diamond(CausalEquations):
 
 class Collider(CausalEquations):
     def __init__(self, eq_type: str):
-        if eq_type == 'linear':
-            functions = [
+        if eq_type == "linear":
+            functions: list[NTen2Ten] = [
                 lambda u1: u1,
-                lambda _, u2: 2.0 - u2,
+                lambda _1, u2: 2.0 - u2,
                 lambda x1, x2, u3: 0.25 * x2 - 0.5 * x1 + 0.5 * u3,
             ]
-            inverses = [
+            inverses: list[NTen2Ten] = [
                 lambda x1: x1,
-                lambda _, x2: 2.0 - x2,
+                lambda _1, x2: 2.0 - x2,
                 lambda x1, x2, x3: (x3 - 0.25 * x2 + 0.5 * x1) / 0.5,
             ]
         else:
@@ -400,49 +417,49 @@ class Collider(CausalEquations):
 
 class Chain3(CausalEquations):
     def __init__(self, eq_type: str):
-        if eq_type == 'linear':
-            functions = [
+        if eq_type == "linear":
+            functions: list[NTen2Ten] = [
                 lambda u1: u1,
                 lambda x1, u2: 10 * x1 - u2,
-                lambda x1, x2, u3: 0.25 * x2 + 2 * u3,
+                lambda _1, x2, u3: 0.25 * x2 + 2 * u3,
             ]
-            inverses = [
+            inverses: list[NTen2Ten] = [
                 lambda x1: x1,
                 lambda x1, x2: (10 * x1 - x2),
-                lambda x1, x2, x3: (x3 - 0.25 * x2) / 2,
+                lambda _1, x2, x3: (x3 - 0.25 * x2) / 2,
             ]
         elif eq_type == 'non-linear':
             functions = [
                 lambda u1: u1,
                 lambda x1, u2: torch.exp(x1 / 2.0) + u2 / 4.0,
-                lambda x1, x2, u3: (x2 - 5) ** 3 / 15.0 + u3,
+                lambda _1, x2, u3: (x2 - 5) ** 3 / 15.0 + u3,
             ]
             inverses = [
                 lambda x1: x1,
                 lambda x1, x2: 4.0 * (x2 - torch.exp(x1 / 2.0)),
-                lambda x1, x2, x3: x3 - (x2 - 5) ** 3 / 15.0,
+                lambda _1, x2, x3: x3 - (x2 - 5) ** 3 / 15.0,
             ]
         elif eq_type == 'non-linear-2':
             functions = [
                 lambda u1: torch.sigmoid(u1),
                 lambda x1, u2: 10 * x1**0.5 - u2,
-                lambda x1, x2, u3: 0.25 * x2 + 2 * u3,
+                lambda _1, x2, u3: 0.25 * x2 + 2 * u3,
             ]
             inverses = [
                 lambda x1: torch.logit(x1),
                 lambda x1, x2: (10 * x1**0.5 - x2),
-                lambda x1, x2, x3: (x3 - 0.25 * x2) / 2,
+                lambda _1, x2, x3: (x3 - 0.25 * x2) / 2,
             ]
         elif eq_type == 'non-linear-3':
             functions = [
                 lambda u1: u1,
                 lambda x1, u2: 1 * x1**2.0 - u2,
-                lambda x1, x2, u3: 0.25 * x2 + 2 * u3,
+                lambda _1, x2, u3: 0.25 * x2 + 2 * u3,
             ]
             inverses = [
                 lambda x1: x1,
                 lambda x1, x2: (1 * x1**2.0 - x2),
-                lambda x1, x2, x3: (x3 - 0.25 * x2) / 2,
+                lambda _1, x2, x3: (x3 - 0.25 * x2) / 2,
             ]
         else:
             raise ValueError(f'Equation type {eq_type} not supported.')
@@ -461,14 +478,14 @@ class Chain3(CausalEquations):
 
 class Chain4(CausalEquations):
     def __init__(self, eq_type: str):
-        if eq_type == 'linear':
-            functions = [
+        if eq_type == "linear":
+            functions: list[NTen2Ten] = [
                 lambda u1: u1,
                 lambda x1, u2: 5 * x1 - u2,
                 lambda _1, x2, u3: -0.5 * x2 - 1.5 * u3,
                 lambda _1, _2, x3, u4: x3 + u4,
             ]
-            inverses = [
+            inverses: list[NTen2Ten] = [
                 lambda x1: x1,
                 lambda x1, x2: (5 * x1 - x2),
                 lambda _1, x2, x3: (-0.5 * x2 - x3) / 1.5,
@@ -492,15 +509,15 @@ class Chain4(CausalEquations):
 
 class Chain5(CausalEquations):
     def __init__(self, eq_type: str):
-        if eq_type == 'linear':
-            functions = [
+        if eq_type == "linear":
+            functions: list[NTen2Ten] = [
                 lambda u1: u1,
                 lambda x1, u2: 10 * x1 - u2,
                 lambda _1, x2, u3: 0.25 * x2 + 2 * u3,
                 lambda _1, _2, x3, u4: x3 + u4,
                 lambda _1, _2, _3, x4, u5: -x4 + u5,
             ]
-            inverses = [
+            inverses: list[NTen2Ten] = [
                 lambda x1: x1,
                 lambda x1, x2: (10 * x1 - x2),
                 lambda _1, x2, x3: (x3 - 0.25 * x2) / 2,
